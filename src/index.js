2b1c6e7ac204dfa336b9ab8bde8946b3a57d9e21
bybybybybyby <bybybybybyby@users.noreply.github.com>
2021-02-22 17:12:32 -0800

    Completed code.

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Bro Skee";
+const currentYear = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {currentYear}</p>
+  </div>,
+  document.getElementById("root")
+);
